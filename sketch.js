ff36f0470ebbaa96295b8e3a31528edee891c010
Aditi <sneemkar@gmail.com>
2021-01-10 12:26:17 +0530

    Debbuged

--- sketch.js
+++ sketch.js
@@ -94,6 +94,8 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+        bird.trajectory=[]
+        Matter.Body.setPosition(bird.body,{x:200,y:50});
        slingshot.attach(bird.body);
     }
 }
